b979fd05159757b8c3f4205e60ded5067a9e371f
Mahesh Dhikonda <mahesh.dhikonda@hotmail.com>
2021-07-21 22:55:26 +0530

    Add project files.

---
+++ UserInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            String Firstname = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            String Lastname = Console.ReadLine();
+            Console.WriteLine("Your full name is "+ Firstname+ " "+Lastname);
+            Console.WriteLine("Your full name is {0} {1}",  Firstname, Lastname);
+            Console.WriteLine($"Your full name is {Firstname} {Lastname}");
+        }
+    }
+}
